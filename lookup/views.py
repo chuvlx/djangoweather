31952b3ceb6263b427f2db8489a835fb9b0cc2bf
Chukwuka Van-lare <chuvl98@gmail.com>
2025-01-10 05:26:08 -0600

    Added a bit of error handling

--- lookup/views.py
+++ lookup/views.py
@@ -1,8 +1,20 @@
 #This is my views.py file
 from django.shortcuts import render
 
+
 def home(request):
-	return render(request, 'home.html', {})
+	import json
+	import requests
+
+	api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=75082&distance=20&API_KEY=2C004E4B-01BF-4362-B814-DEA6EB3F1A2A")
+	error_msg = "Oops we have an error.."
+
+	try:
+		api = json.loads(api_request.content)
+	except Exception as e:
+		api = error_msg
+
+	return render(request, 'home.html', {'api' : api})
 
 def about(request):
 	return render(request, 'about.html', {})
